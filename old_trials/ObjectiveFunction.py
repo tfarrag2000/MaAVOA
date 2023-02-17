b85201b0a0dbf7e083f59bcca905ca50804cc9b2
Tamer A. Farrag, PhD <tfarrag2000@hotmail.com>
2023-02-17 11:21:51 +0300

    Delete old_trials directory

--- old_trials/ObjectiveFunction.py
+++
@@ -1,8 +0,0 @@
-import numpy
-
-
-def ObjectiveFunction(x):
-    # change this file according to your objective function
-    o = sum(numpy.power(x, 2))
-    # print(o)
-    return o
--- old_trials/rouletteWheelSelection.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-
-def rouletteWheelSelection(x):
-    index = np.nonzero(np.random.rand() <= np.cumsum(x))
-    index1 = index[0][0]
-    return index1
